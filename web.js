18b6fba4216499678953c3b730a03d82b886f0e1
goran <goran@nordicwingsail.com>
2013-07-12 03:11:38 +0000

    new try

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var readFile = fs.readFileSync('index.html', 'utf-8'); 
-var readBuffer = new Buffer(readFile);
+var readBuffer = new Buffer(1024);
 readBuffer.write(readFile, 0);
 app.get('/', function(request, response) {
   response.send(readBuffer.toString('utf8', 0, readBuffer.length));
