5d089b6411a70e10c230752899322e773ac0bb26
goran <goran@nordicwingsail.com>
2013-07-12 02:41:32 +0000

    Updated web.js to read from file

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
+var fs = require('fs');
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var readFile = fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+var readBuffer = new Buffer(1024);
+readBuffer.write(readFile, 0);
+app.get('/', function(request, response) {
+  response.send(readBuffer.toString('utf8', 0, readBuffer.length));
 });
