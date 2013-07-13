89a9f9f093460691d5cd3510b994b22f80065965
goran <goran@nordicwingsail.com>
2013-07-13 12:46:55 +0000

    updated the code

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
 var express = require('express');
 var app = express.createServer(express.logger());
-
-var readFile = fs.readFileSync('index.html', 'utf-8'); 
-var readBuffer = new Buffer(1024);
-readBuffer.write(readFile, 0);
-app.get('/', function(request, response) {
+var theFile = "index.html";
+var readFile = fs.readFileSync(theFile, 'utf-8'); 
+var readBuffer = new Buffer(readFile);
+//readBuffer.write(readFile, 0);
+app.get('/', function (request, response) {
   response.send(readBuffer.toString('utf8', 0, readBuffer.length));
 });
