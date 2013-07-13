fd11396cde701dc92f6c10729cde5978fbaaf447
goran <goran@nordicwingsail.com>
2013-07-13 23:35:25 +0000

    updated the code

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
-var app = express.createServer(express.logger());
+var app = express(express.logger());
 var filePath = path.join(__dirname, 'index.html');
+console.log(filePath);
 //var theFile = "index.html";
 var readFile = fs.readFileSync(filePath); 
+
 //var readBuffer = new Buffer(readFile);
 //readBuffer.write(readFile, 0);
 app.get('/', function (request, response) {
-   // console.log(response);
+   console.log(response);
   response.send(readFile.toString('utf8', 0, readBuffer.length));
 });
